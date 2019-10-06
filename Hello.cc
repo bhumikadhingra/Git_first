bf5a2d7c92fc1710b5fb4e6b06e22580a2f6657d
Bhumika Dhingra <51711116+bhumikadhingra@users.noreply.github.com>
2019-10-06 18:43:22 +0530

    Update Hello.cc

--- Hello.cc
+++ Hello.cc
@@ -1,9 +1,21 @@
-#include <iostream>
-using namespace std;
-int main (){
+// #include <iostream>
+// using namespace std;
+// int main (){
+// 	cout << "hello world";
+// 	cout << "/n"; // New line character
+// 	cout<<"This is Neil";
+	
+// 	return 0;
+// }
+
+// ------------OR-------------------
+
+#include<iostream.h>         
+#include<conio.h>            // used for clrscr()
+void main(){
+	clrscr();
 	cout << "hello world";
 	cout << "/n"; // New line character
-	cout<<"This is Neil";
+	cout<<"This is Bhumika";
 	
-	return 0;
-}
+}	
